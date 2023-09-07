b3a72cda6f5aa967f4befa35d32fc9575dd71a78
sun20886 <ssun20886@gmail.com>
2023-09-07 19:05:52 +0900

    js array

---
+++ class/03-javascript/연습-01-array.js
@@ -0,0 +1,45 @@
+let classmates=["철수", "영희", "훈이"]
+// undefined
+classmates
+// (3) ['철수', '영희', '훈이']
+classmates[0]
+// '철수'
+classmates[1]
+// '영희'
+classmates[2]
+// '훈이'
+classmates.includes("훈이")
+// true
+classmates.includes("맹구")
+// false
+classmates.length
+// 3
+classmates.push("맹구")
+// 4
+classmates.includes("맹구")
+// true
+classmates
+// (4) ['철수', '영희', '훈이', '맹구']
+classmates.length
+// 4
+classmates.pop()
+// '맹구'
+classmates.length
+// 3
+classmates
+// (3) ['철수', '영희', '훈이']
+////////////////////////////////////////////////////////
+let developer=["aa","bb", "cc", "dd", "ee"]
+// undefined
+developer[3]
+// 'dd'
+let dream=["AA", "BB", "CC"]
+// undefined
+developer.concat(dream)
+// (8) ['aa', 'bb', 'cc', 'dd', 'ee', 'AA', 'BB', 'CC']
+let result=developer.concat(dream)
+// undefined
+result
+// (8) ['aa', 'bb', 'cc', 'dd', 'ee', 'AA', 'BB', 'CC']
+
+////////////////////////////////////
