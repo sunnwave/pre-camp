39673666ae3bf86cf79d1a7b265c96eee260632a
sun20886 <ssun20886@gmail.com>
2023-09-13 20:14:54 +0900

    싸이월드 4일차 수정

--- class/03-javascript/06-timer.js
+++ class/03-javascript/06-timer.js
@@ -1,18 +1,30 @@
+let isStarted=false
+
 sendAuthNum=()=>{
-    let time=180
-    let authNum=String(Math.floor(Math.random()*1000000)).padStart(6,'0')
+    if (isStarted===false){
+        //타이머가 작동중이 아닐 때
+        isStarted=true
+        document.getElementById("success").disabled=false
+        let time=180
+        let timer
 
-    document.getElementById("authNum").innerText=authNum
-    setInterval(() => {
-        if (time>=0){
-            let min=String(Math.floor(time/60))
-            let sec=String(time%60).padStart(2,"0")
-            document.getElementById("timer").innerText=min+":"+sec
-            time=time-1
-        } else{
-            document.getElementById("success").disabled=true
-        }
-    }, 1000);
+        let authNum=String(Math.floor(Math.random()*1000000)).padStart(6,'0')
+        document.getElementById("authNum").innerText=authNum
 
-    
+        timer=setInterval(() => {
+            if (time>=0){
+                let min=String(Math.floor(time/60))
+                let sec=String(time%60).padStart(2,"0")
+                document.getElementById("timer").innerText=min+":"+sec
+                time=time-1
+            } else{
+                document.getElementById("success").disabled=true
+                isStarted=false
+                clearInterval(timer)
+            }
+        }, 1000);
+
+    }else{
+        //타이머가 작동중일 때
+    }    
 }
